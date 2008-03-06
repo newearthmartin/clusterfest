33faba203178077987d476ab1fc60502826cdee0
martinmassera <martinmassera@d983c191-aa41-0410-9f70-3341a9aa1916>
2008-03-06 21:23:08 +0000

    removed packages renamed clustering package to clusterfest

---
+++ trunk/src/com/flaptor/clusterfest/monitoring/node/Monitoreable.java
@@ -0,0 +1,59 @@
+/*
+Copyright 2008 Flaptor (flaptor.com) 
+
+Licensed under the Apache License, Version 2.0 (the "License"); 
+you may not use this file except in compliance with the License. 
+You may obtain a copy of the License at 
+
+    http://www.apache.org/licenses/LICENSE-2.0 
+
+Unless required by applicable law or agreed to in writing, software 
+distributed under the License is distributed on an "AS IS" BASIS, 
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+See the License for the specific language governing permissions and 
+limitations under the License.
+*/
+
+package com.flaptor.clusterfest.monitoring.node;
+
+import java.util.Map;
+
+import com.flaptor.clusterfest.monitoring.SystemProperties;
+
+/**
+ * interface for monitoring nodes
+ *
+ * @author Martin Massera
+ */
+public interface Monitoreable {
+	
+	/**
+	 * pinging method for determining if the node is monitoreable
+	 * @return true
+	 */
+	public boolean ping() throws Exception;
+	
+	/**
+	 * @return a map of property -> value
+	 * 
+	 */
+	public Map<String, Object> getProperties()  throws Exception;
+	
+	/**
+	 * @param property the property to get a value for
+	 * @return the value (can be any type of object)
+	 */
+	public Object getProperty(String property) throws Exception;
+	
+	/**
+	 * @return the system properties
+	 */
+	public SystemProperties getSystemProperties() throws Exception;
+	
+	/**
+	 * gets a log from the node
+	 * @param logName the name of the log to be retrieved
+	 * @return the log
+	 */
+	public String getLog(String logName) throws Exception;
+}
