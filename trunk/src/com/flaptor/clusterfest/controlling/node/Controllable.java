eb8d9379f32d2c45d17f408ce5f20b9017146c4a
martinmassera <martinmassera@d983c191-aa41-0410-9f70-3341a9aa1916>
2008-03-06 21:20:39 +0000

    removed packages renamed clustering package to clusterfest

---
+++ trunk/src/com/flaptor/clusterfest/controlling/node/Controllable.java
@@ -0,0 +1,53 @@
+/*
+Copyright 2008 Flaptor (flaptor.com) 
+
+Licensed under the Apache License, Version 2.0 (the "License"); 
+you may not use this file except in compliance with the License. 
+You may obtain a copy of the License at 
+
+    http://www.apache.org/licenses/LICENSE-2.0 
+
+Unless required by applicable law or agreed to in writing, software 
+distributed under the License is distributed on an "AS IS" BASIS, 
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+See the License for the specific language governing permissions and 
+limitations under the License.
+*/
+
+package com.flaptor.clusterfest.controlling.node;
+
+import com.flaptor.clusterfest.controlling.ControllerNodeState;
+
+/**
+ * interface for controlling nodes through rpc
+ *
+ * @author Martin Massera
+ */
+public interface Controllable {
+
+	/**
+	 * pinging method for determining if the node is controllable
+	 * @return true
+	 */
+	public boolean ping() throws Exception;
+	
+	/**
+	 * @return the state of the node
+	 */
+	public ControllerNodeState getState() throws Exception;
+	
+	/**
+	 * pause the service
+	 */
+	public void pause() throws Exception;
+
+	/**
+	 * resume the service
+	 */
+	public void resume() throws Exception;
+	
+	/**
+	 * stop and exit the process gently
+	 */
+	public void stop() throws Exception;
+}
