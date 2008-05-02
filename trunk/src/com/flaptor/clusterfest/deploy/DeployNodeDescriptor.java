ea5e36828bf7d9c138dbeaa9c5285ec65d1d75b7
martinmassera <martinmassera@d983c191-aa41-0410-9f70-3341a9aa1916>
2008-05-02 20:18:43 +0000

    refactor

---
+++ trunk/src/com/flaptor/clusterfest/deploy/DeployNodeDescriptor.java
@@ -0,0 +1,24 @@
+package com.flaptor.clusterfest.deploy;
+
+import com.flaptor.clusterfest.ModuleNodeDescriptor;
+import com.flaptor.clusterfest.NodeDescriptor;
+import com.flaptor.clusterfest.NodeUnreachableException;
+import com.flaptor.clusterfest.monitoring.MonitorModule;
+
+/**
+ * Base class for nodes of the action module
+ * @author Martin Massera
+ */
+public class DeployNodeDescriptor extends ModuleNodeDescriptor{
+
+    private DeployListener deployListener;
+    
+    public DeployNodeDescriptor(NodeDescriptor nodeDescriptor) {
+        super(nodeDescriptor);
+        deployListener = DeployModule.getModuleListener(nodeDescriptor.getXmlrpcClient());
+    }
+    
+    public void deployFile(String path, String filename, byte[] content) throws Exception {
+        deployListener.deployFile(path, filename, content);
+    }
+}
